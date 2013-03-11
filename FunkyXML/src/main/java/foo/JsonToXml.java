6c23a412a39819ca7c8d85929e9988c55b76b3bc
Damien Arondel <damien.arondel@free.fr>
2013-03-11 23:28:24 +0100

    Màj des fichiers Java pour supprimer les erreurs Checkstyle

--- FunkyXML/src/main/java/foo/JsonToXml.java
+++ FunkyXML/src/main/java/foo/JsonToXml.java
@@ -6,20 +6,36 @@ import java.io.InputStream;
 import org.json.JSONObject;
 import org.json.XML;
 
-public class JsonToXml {
-	public static String convert(String jsonFile) {
-		try {
-			InputStream is = JsonToXml.class.getResourceAsStream(jsonFile);
-			int ptr = 0;
-			StringBuilder builder = new StringBuilder();
-			while ((ptr = is.read()) != -1) {
-			    builder.append((char) ptr);
-			}
-			JSONObject jsonObject = new JSONObject(builder.toString());
-			return XML.toString(jsonObject);
-		} catch (IOException e) {
-			System.err.format("IOException: %s%n", e);
-			return null;
-		}
-	}
+/**
+ * Provide a JSON to XML converter.
+ * @version 2013-03-11
+ */
+public final class JsonToXml {
+    /**
+     * Empty private constructor (utility class).
+     */
+    private JsonToXml() {
+    }
+
+    /**
+     * Convert a JSON file into an XML one, according to its path.
+     * @param jsonFile
+     *            Path of the JSON file.
+     * @return The content of the XML file.
+     */
+    public static String convert(final String jsonFile) {
+        try {
+            InputStream is = JsonToXml.class.getResourceAsStream(jsonFile);
+            int ptr = 0;
+            StringBuilder builder = new StringBuilder();
+            while ((ptr = is.read()) != -1) {
+                builder.append((char) ptr);
+            }
+            JSONObject jsonObject = new JSONObject(builder.toString());
+            return XML.toString(jsonObject);
+        } catch (IOException e) {
+            System.err.format("IOException: %s%n", e);
+            return null;
+        }
+    }
 }
--- FunkyXML/src/main/java/foo/XmlToJson.java
+++ FunkyXML/src/main/java/foo/XmlToJson.java
@@ -6,21 +6,42 @@ import java.io.InputStream;
 import org.json.JSONObject;
 import org.json.XML;
 
-public class XmlToJson {
-	public static String convert(String xmlFile) {
-		try {
-			InputStream is = XmlToJson.class.getResourceAsStream(xmlFile);
-			int ptr = 0;
-			StringBuilder builder = new StringBuilder();
-			while ((ptr = is.read()) != -1) {
-			    builder.append((char) ptr);
-			}
-			String xml = builder.toString();
-			JSONObject jsonObject = XML.toJSONObject(xml);
-			return jsonObject.toString(4);
-		} catch (IOException e) {
-			System.err.format("IOException: %s%n", e);
-			return null;
-		}
-	}
+/**
+ * Provide an XML to JSON converter.
+ * @version 2013-03-11
+ */
+public final class XmlToJson {
+    /**
+     * Number of indent spaces used in the JSON file.
+     */
+    private static final int INDENT_SPACES_NUMBER = 4;
+
+    /**
+     * Empty private constructor (utility class).
+     */
+    private XmlToJson() {
+    }
+
+    /**
+     * Convert an XML file into a JSON one, according to its path.
+     * @param xmlFile
+     *            Path of the XML file.
+     * @return The content of the JSON file.
+     */
+    public static String convert(final String xmlFile) {
+        try {
+            InputStream is = XmlToJson.class.getResourceAsStream(xmlFile);
+            int ptr = 0;
+            StringBuilder builder = new StringBuilder();
+            while ((ptr = is.read()) != -1) {
+                builder.append((char) ptr);
+            }
+            String xml = builder.toString();
+            JSONObject jsonObject = XML.toJSONObject(xml);
+            return jsonObject.toString(INDENT_SPACES_NUMBER);
+        } catch (IOException e) {
+            System.err.format("IOException: %s%n", e);
+            return null;
+        }
+    }
 }
