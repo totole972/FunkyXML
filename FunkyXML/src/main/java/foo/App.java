c2424835a1dee5da19a050e33f97b11fd8b0f597
Damien Arondel <damien.arondel@free.fr>
2013-03-11 16:50:11 +0100

    Main mis à jour pour montrer le code de XML to JSON

--- FunkyXML/src/main/java/foo/App.java
+++ FunkyXML/src/main/java/foo/App.java
@@ -1,11 +1,19 @@
 package foo;
 
+import net.sf.json.JSON;
+
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+    	String xmlFileName = "blabla.xml";
+    	JSON json = XmlToJson.convert(xmlFileName);
+    	boolean isSaved = FileSaver.save("blabla.json", json.toString(2));
+        if (isSaved)
+        	System.out.println("Conversion optimale!");
+        else
+        	System.out.println("C'est nul!");
     }
 }
