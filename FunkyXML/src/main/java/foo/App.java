9f182c4095a55bb7e7849182407f79d1c617b76f
Damien Arondel <damien.arondel@free.fr>
2013-03-11 23:33:44 +0100

    #4 Recommit Julien

--- FunkyXML/src/main/java/foo/App.java
+++ FunkyXML/src/main/java/foo/App.java
@@ -1,25 +1,63 @@
 package foo;
 
-public class App {
-    public static void main(String[] args) {
-    	String xmlFileName = "quiz-moodle-exemple.xml";
-    	String jsonFileName = "quiz-moodle-exemple.json";
-    	
-    	String jsonContent = XmlToJson.convert(xmlFileName);
-    	boolean isSaved = FileSaver.save(jsonFileName, jsonContent);
-        if (isSaved) {
-        	System.out.println("XML -> JSON : Conversion optimale!");
-        	
-	        String xmlContent = JsonToXml.convert(jsonFileName);
-	        isSaved = FileSaver.save(xmlFileName, xmlContent);
-	        if (isSaved)
-	            System.out.println("JSON -> XML : Conversion jsonxml ok!");
-	        else
-	            System.out.println("JSON -> XML : Conversion jsonxml echec!");
-        }
-        else
-        	System.out.println("XML -> JSON : C'est nul!");
+import java.io.File;
 
-        /* */
+public class App {
+
+    /**
+     * 
+     * @param args : 
+     *          args[0] = {xml;json}: définit le format d'entré
+     *          args[1] : définit le fichier d'entré
+     *          args[2] : définit le fichier de sortie, console si vide
+     */
+    public static void main(String[] args) {
+        if (!checkParam(args))
+            return;
+                
+        String InputFileName = args[1];
+        String output = null;
+
+        if (args[0].toUpperCase().equals("XML")){
+            String json = XmlToJson.convert(InputFileName);
+            if (json == null){
+                System.err.println("erreur dans la conversion du fichier");
+            }else
+                output = json;
+        }else if (args[0].toUpperCase().equals("JSON")){
+            output = JsonToXml.convert(InputFileName);
+        }
+        
+        //affichage ou sauvegarde
+        if (args.length == 3){
+            boolean isSaved = FileSaver.save(args[2], output);
+            if (!isSaved){
+                System.err.println("erreur à l'ecriture du fichier!");
+                System.out.println(output);
+            }       
+        }else
+            System.out.println(output);
+    }
+
+
+    private static boolean checkParam(String[] args) {
+        if ( !(args[0].toLowerCase().equals("xml") || args[0].toLowerCase().equals("json")) || args.length<2 || args.length>3 || !args[1].toLowerCase().endsWith(args[0].toLowerCase())
+        || !new File(args[1]).canRead()){
+            showHelp();
+            System.out.println(!args[1].toLowerCase().endsWith("." + args[0].toLowerCase()));
+            return false;
+        }else if(args.length == 3 && !(args[2].toUpperCase().endsWith(args[0].toLowerCase()) && new File(args[2]).canWrite())){
+            showHelp();
+            return false;
+        }else
+            return true;
+    }
+
+
+    private static void showHelp() {
+        System.out.println("args[0] = {xml;json}: définit le format d'entré\n" +
+            "args[1] : définit le fichier d'entré\n" +
+            "args[2] : définit le fichier de sortie, console si vide");
+
     }
 }
