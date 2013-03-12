d5b4f25dd412d12ffc054b60c825387f9a71f496
Kevin ANATOLE <kevinanatole@yahoo.fr>
2013-03-12 20:24:59 +0100

    Correction du programme principal pour la prise en compte des trois paramètres(susceptible d'être modifié ultérieurement)

--- FunkyXML/src/main/java/foo/App.java
+++ FunkyXML/src/main/java/foo/App.java
@@ -7,7 +7,7 @@ public class App {
 	/**
 	 * 
 	 * @param args : 
-	 * 			args[0] = {xml;json}: définit le format d'entré
+	 * 			args[0] = {xml;json}: définit le format de sortie
 	 * 			args[1] : définit le fichier d'entré
 	 * 			args[2] : définit le fichier de sortie, console si vide
 	 */
@@ -19,9 +19,9 @@ public class App {
 		String output = null;
 		
 		if (args[0].toUpperCase().equals("XML")){
-			output = XmlToJson.convert(InputFileName).toString();
+            output = JsonToXml.convert(InputFileName);
 		}else if (args[0].toUpperCase().equals("JSON")){
-			output = JsonToXml.convert(InputFileName);
+            output = XmlToJson.convert(InputFileName).toString();
 		}
     	
 		//affichage ou sauvegarde
@@ -36,13 +36,13 @@ public class App {
 	}
 	
 
-	private static boolean checkParam(String[] args) {
-		if ( !(args[0].toLowerCase().equals("xml") || args[0].toLowerCase().equals("json")) || args.length<2 || args.length>3 || !args[1].toLowerCase().endsWith(args[0].toLowerCase())
+	public static boolean checkParam(String[] args) {
+		if ( !(args[0].toLowerCase().equals("xml") || args[0].toLowerCase().equals("json")) || args.length<2 || args.length>3 || !args[2].toLowerCase().endsWith(args[0].toLowerCase())
 		|| !new File(args[1]).canRead()){
 			showHelp();
 			System.out.println(!args[1].toLowerCase().endsWith("." + args[0].toLowerCase()));
 			return false;
-		}else if(args.length == 3 && !(args[2].toUpperCase().endsWith(args[0].toLowerCase()) && new File(args[2]).canWrite())){
+		}else if(args.length == 3 && (!args[2].toLowerCase().endsWith(args[0].toLowerCase())/* ||  !new File(args[2]).canWrite()*/)){
 			showHelp();
 			return false;
 		}else
@@ -51,7 +51,7 @@ public class App {
 	
 	
 	private static void showHelp() {
-		System.out.println("args[0] = {xml;json}: définit le format d'entré\n" +
+		System.out.println("args[0] = {xml;json}: définit le format de sortie\n" +
 			"args[1] : définit le fichier d'entré\n" +
 			"args[2] : définit le fichier de sortie, console si vide");
 		
